c5697c882fe20f8bdd74362057f2dede6598544b
home <gisaq@yandex.ru>
2015-07-23 12:42:48 +0300

    added launch view controller

---
+++ VTableView/VTableView/ViewController.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+using Foundation;
+using UIKit;
+
+namespace VTableView
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController () : base ("ViewController", null)
+		{
+		}
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+		}
+	}
+}
+
